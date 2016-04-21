20d05a5719e540d141d7db03f97bff4e0e7424e9
Mauro Marano <gagginaspinnata@MBP-di-Mauro.homenet.telecomitalia.it>
2016-04-21 15:36:55 +0200

    refactored

--- app.js
+++ app.js
@@ -16,20 +16,21 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var Alerter = function () {
     function Alerter() {
-        var _this = this;
-
         _classCallCheck(this, Alerter);
 
         this.top_stories_url = 'https://hacker-news.firebaseio.com/v0/topstories.json';
         this.story_url = 'https://hacker-news.firebaseio.com/v0/item';
-        this.min_score = 300;
+        // this.min_score = 300;
 
-        this.get_stories(function (stories) {
-            _this.stories_with_score(stories, 10, function (result) {
-                console.log(result);
-            });
-        });
+        // this.get_stories((stories) => {
+        //     this.stories_with_score(stories, 10, (result) => {
+        //         console.log(result);
+        //     })
+        // });
     }
+
+    // get the top stories on YC and parse them as json to a cb
+
 
     _createClass(Alerter, [{
         key: "get_top_stories_id",
@@ -39,6 +40,21 @@ var Alerter = function () {
                 cb(JSON.parse(body));
             });
         }
+
+        //   get a single story and parse them into a cb as json
+        //   Example of story
+        //     {
+        //   "by" : "dhouston",
+        //   "descendants" : 71,
+        //   "id" : 8863,
+        //   "kids" : [ 8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876 ],
+        //   "score" : 111,
+        //   "time" : 1175714200,
+        //   "title" : "My YC app: Dropbox - Throw away your USB drive",
+        //   "type" : "story",
+        //   "url" : "http://www.getdropbox.com/u/2/screencast.html"
+        // }
+
     }, {
         key: "get_story",
         value: function get_story(url, cb) {
@@ -48,19 +64,19 @@ var Alerter = function () {
             });
         }
 
-        //when this is done the global var list_of_stories
-        // will have an array with all the top stories
+        // get the top stories cycle them into the get_story function
+        // pass trougth a cb an array of stories
 
     }, {
         key: "get_stories",
         value: function get_stories(cb) {
-            var _this2 = this;
+            var _this = this;
 
             var result = [];
             this.get_top_stories_id(function (stories_id) {
                 _async2.default.each(stories_id, function (story_id, callback) {
-                    var url = _this2.story_url + "/" + story_id + ".json";
-                    _this2.get_story(url, function (data) {
+                    var url = _this.story_url + "/" + story_id + ".json";
+                    _this.get_story(url, function (data) {
                         result.push(data);
                         callback(null);
                     });
@@ -70,21 +86,25 @@ var Alerter = function () {
                 });
             });
         }
+
+        // Get all the stories from the top one's
+        // And filter them passing only the story with a minimun score
+        // pass an array of story into the cb
+
     }, {
         key: "stories_with_score",
-        value: function stories_with_score(stories, score, cb) {
-            var _this3 = this;
-
+        value: function stories_with_score(score, cb) {
             var result = [];
-            _async2.default.each(stories, function (story, callback) {
-                if (story.score >= _this3.min_score) {
-                    result.push(story);
-                }
-                callback(null);
-            }, function (err) {
-                if (err) throw err;
-
-                cb(result);
+            this.get_stories(function (stories) {
+                _async2.default.each(stories, function (story, callback) {
+                    if (story.score >= score) {
+                        result.push(story);
+                    }
+                    callback(null);
+                }, function (err) {
+                    if (err) throw err;
+                    cb(result);
+                });
             });
         }
     }]);
@@ -93,3 +113,12 @@ var Alerter = function () {
 }();
 
 var alerter = new Alerter();
+alerter.stories_with_score(500, function (stories) {
+    _async2.default.each(stories, function (story, cb) {
+        console.log(story.title + " url: " + story.url);
+        cb();
+    }, function (err, res) {
+        if (err) throw err;
+        console.log('done');
+    });
+});
--- src/app.js
+++ src/app.js
@@ -5,17 +5,9 @@ class Alerter {
     constructor() {
         this.top_stories_url = 'https://hacker-news.firebaseio.com/v0/topstories.json';
         this.story_url = 'https://hacker-news.firebaseio.com/v0/item';
-    	this.min_score = 300;
-
-        this.get_stories((stories)=>{
-        	this.stories_with_score(stories,10,(result)=>{
-        		console.log(result);
-        	})
-        });
-
-
     }
 
+    // get the top stories on YC and parse them as json to a cb
     get_top_stories_id(cb) {
         request.get(this.top_stories_url, (err, resp, body) => {
             if (err) throw err;
@@ -23,6 +15,19 @@ class Alerter {
         });
     }
 
+    //   get a single story and parse them into a cb as json
+    //   Example of story
+    //     {
+    //   "by" : "dhouston",
+    //   "descendants" : 71,
+    //   "id" : 8863,
+    //   "kids" : [ 8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876 ],
+    //   "score" : 111,
+    //   "time" : 1175714200,
+    //   "title" : "My YC app: Dropbox - Throw away your USB drive",
+    //   "type" : "story",
+    //   "url" : "http://www.getdropbox.com/u/2/screencast.html"
+    // }
     get_story(url, cb) {
         request.get(url, (err, resp, body) => {
             if (err) throw err;
@@ -30,10 +35,10 @@ class Alerter {
         });
     }
 
-    //when this is done the global var list_of_stories
-    // will have an array with all the top stories
+    // get the top stories cycle them into the get_story function
+    // pass trougth a cb an array of stories
     get_stories(cb) {
-    	let result = []
+        let result = []
         this.get_top_stories_id((stories_id) => {
             async.each(stories_id, (story_id, callback) => {
                 let url = `${this.story_url}/${story_id}.json`
@@ -43,26 +48,38 @@ class Alerter {
 
                 });
             }, (err) => {
-            	if (err) throw err;
+                if (err) throw err;
                 cb(result);
             });
         });
     }
 
-    stories_with_score(stories,score,cb){
-    	let result = [];
-    	async.each(stories,(story,callback)=>{
-    		if(story.score>=this.min_score){
-    			result.push(story);
-    		}
-    		callback(null);
-    	},
-    	(err)=>{
-    		if(err)throw err;
-
-    		cb(result)
-    	});
+    // Get all the stories from the top one's
+    // And filter them passing only the story with a minimun score
+    // pass an array of story into the cb
+    stories_with_score(score, cb) {
+        let result = [];
+        this.get_stories((stories) => {
+            async.each(stories, (story, callback) => {
+                if (story.score >= score) {
+                    result.push(story);
+                }
+                callback(null);
+            }, (err) => {
+                if (err) throw err;
+                cb(result)
+            });
+        });
     }
 }
 
-let alerter = new Alerter();
+// let alerter = new Alerter();
+// alerter.stories_with_score(500, (stories) => {
+//     async.each(stories, (story, cb) => {
+//         console.log(`${story.title} url: ${story.url}`);
+//         cb();
+//     }, (err, res) => {
+//         if (err) throw err;
+//         console.log('done');
+//     });
+// });
